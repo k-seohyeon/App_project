65e129301c83891ed0a889dce3a88b2ec78a4af8
k-seohyseon <kangsh55179119@gmail.com>
2025-11-17 14:43:08 +0900

    layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack 
-      initialRouteName="index" screenOptions={{headerShown: false}}>
+      initialRouteName="index" screenOptions={{headerShown: false}}> // 상단 바 없애기
         <Stack.Screen name="index"/>
         <Stack.Screen name="home"/>
     </Stack>
